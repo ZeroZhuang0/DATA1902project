0c5b80ae0d76fae0a60eee649369dd7749bf194b
sashajenner <48614064+sashajenner@users.noreply.github.com>
2019-10-11 14:06:39 +1100

    Update gold modified

--- code/cleaning/final_clean.py
+++ code/cleaning/final_clean.py
@@ -1,4 +1,4 @@
-    ## Importing necessary modules and libraries
+    # Importing necessary modules and libraries
 import checks_clean # For cleaning checks
 import pandas as pd # For working with the datasets
 
@@ -6,3 +6,5 @@ import pandas as pd # For working with the datasets
 df_gold = pd.read_csv("../../datasets/modified/gold_mod.csv", index_col = False)
 df_boston_crimes = pd.read_csv("../../datasets/modified/boston_crimes_agg.csv", index_col = False)
 df_bitcoin_tweets = pd.read_csv("../../datasets/modified/bitcoin_tweets_agg.csv", index_col = False)
+
+
