c4a2d65eb48237a9bb73ae4ddf0b6b8322637ddc
sashajenner <48614064+sashajenner@users.noreply.github.com>
2019-10-11 16:19:28 +1100

    Update zero_gold_day.py

--- code/cleaning/zero_gold_day.py
+++ code/cleaning/zero_gold_day.py
@@ -1,3 +1,49 @@
+import csv
+import pandas as pd
+from datetime import datetime, date,timedelta
+df = pd.read_csv("https://raw.githubusercontent.com/ZeroZhuang0/DATA1902project/master/datasets/gold.csv")
+def string_to_date(s):
+  year=s.split("-")[0]
+  month=s.split("-")[1]
+  day=s.split("-")[2] 
+  return date(year = int(year) , month = int(month), day = int(day))
+i=0
+pre=None
+result = pd.DataFrame(columns=["Date","Value"])
+one_day=timedelta(days=1)
+while i < len(df["Date"]):
+  if pre == None:
+    pre=df["Date"][i]
+    value=df["Value"][i]
+    result.loc[len(result['Date'])]=[pre,value]
+    pre=string_to_date(df["Date"][i])
+  else:
+    current_date=string_to_date(df["Date"][i])
+    if(current_date-pre).total_seconds()==-86400:
+      result.loc[len(result['Date'])]=[df["Date"][i],df["Value"][i]]
+      pre=current_date
+      value=df["Value"][i]
+    else:
+      pre=pre-one_day
+      result.loc[len(result['Date'])]=[pre.isoformat(),value]
+      pre=pre-one_day
+      result.loc[len(result['Date'])]=[pre.isoformat(),value]
+  i+=1
+n=len(df["Date"])
+result.loc[len(result['Date'])]=[df["Date"][n-1],df["Value"][n-1]]
+result2 = pd.DataFrame(columns=["Date","Value"])
+j=0
+while j< len(result["Date"]):
+  result2.loc[(len(result["Date"])-j)]=[result["Date"][j],result["Value"][j]]
+  j+=1
+result2=result2.iloc[::-1]
+print(result2)
+
+
+
+#-----------------------------------------------------------------------------------------
+
+
 # Defining a function that converts a string to a date object
 def string_to_date(date_string):
     date_splitted = date_string.split("-")
