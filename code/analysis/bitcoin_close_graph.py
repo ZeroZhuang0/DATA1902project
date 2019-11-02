34c3a5ca92bd2a0300f01b25743d98ee4ee7abfc
sashajenner <48614064+sashajenner@users.noreply.github.com>
2019-11-02 12:26:48 +1100

    Code

--- code/analysis/bitcoin_close_graph.py
+++ code/analysis/bitcoin_close_graph.py
@@ -1,4 +1,5 @@
     ## Importing necessary libraries
+
 import pandas as pd
 
 import matplotlib.pyplot as plt
@@ -12,25 +13,29 @@ import descartes
 import geopandas as gpd
 from shapely.geometry import Point
 
+import seaborn as sns
+
+
 # Reading the dataframe from csv file
 df = pd.read_csv("../../datasets/final/df_combined.csv", index_col = False)
 
-bitcoin_close = df["close"]
-time = df["date"]
 
-#fig, ax = plt.subplots()
-##plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-##plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=5))
-#ax.scatter(time, bitcoin_close)
-#ax.set_title("Bitcoin close price from 2017-08-01 to 2019-21-01")
-#ax.set_xlabel("Time")
-#ax.set_ylabel("Bitcoin close prices (" + u"\u20BF" + ")")
-##plt.gcf().autofmt_xdate()
-#plt.show()
+'''
+Bitcoin Close Price VS Date
+Coloured by Volume of Bitcoin Tweets
+Size by Number of Financial Crimes
+'''
 
-# TODO: Change legend title to "Total Volume of Tweets"
-fig = px.scatter(df, x = "date", y = "close", color = "total_volume_of_tweets", size = "financial_crimes")
-#fig.add_trace(go.Scatter(x = df["date"], y = df["close"], opacity = 0.3))
+price_date = go.Scatter(x = df.date, y = df.close,
+        mode = "markers", 
+        marker = dict(
+            colorbar = dict(title = "Number of Tweets"),
+            color = df.total_volume_of_tweets,
+            size = df.financial_crimes 
+            )
+        )
+
+fig = go.Figure(data = [price_date])
 fig.update_layout(title = "Daily Bitcoin Close Price Between 2017-08-01 and 2019-01-21",
         xaxis_title = "Date",
         yaxis_title = "Bitcoin Closing Price ({})".format(u"\u0243"))
@@ -45,6 +50,35 @@ pio.write_html(fig, "../../diagrams/time_vs_bitcoin_close_price_vs_volume_tweets
 fig = px.scatter_3d(df, x = "bitcoin_close_change", y = "gold_change", z = "total_crimes", color = "total_volume_of_tweets") 
 #fig.show()
 pio.write_html(fig, "../../diagrams/bitcoin_close_price_change_vs_gold_price_change_vs_total_crime.html")
+
+fig = px.scatter_3d(df, x = "gold_value", y = "num_domestic", z = "total_volume_of_tweets", color = "gold_change") 
+#fig.show()
+pio.write_html(fig, "../../diagrams/gold_value_vs_domestic_crimes_vs_total_tweets.html")
+
+
+color_continuous_scale = [[0.0, "rgb(171,217,233)"],
+        [0.1111111111111111, "rgb(198,230,241)"],
+        [0.2222222222222222, "rgb(224,243,248)"],
+        [0.3333333333333333, "rgb(239,234,196)"],
+        [0.4444444444444444, "rgb(254,224,144)"],
+        [0.5555555555555556, "rgb(254,199,121)"],
+        [0.6666666666666666, "rgb(253,174,97)"],
+        [0.7777777777777778, "rgb(244,109,67)"],
+        [0.8888888888888888, "rgb(215,48,39)"],
+        [1.0, "rgb(165,0,38)"]]
+
+fig = px.scatter(df, y = "num_domestic", x = "bitcoin_close_change", size = "financial_crimes", color = "total_volume_of_tweets", color_continuous_scale = "Rainbow")
+fig.update_traces(marker=dict(line = dict(
+				width = 0)),
+                  selector=dict(mode='markers'))
+#fig.update_layout(plot_bgcolor = "rgb(150, 150, 150)")
+pio.write_html(fig, "../../diagrams/bitcoin_close_price_vs_domestic_crimes.html")
+
+
+fig = px.scatter_3d(df, x = "bitcoin_close_change", y = "gold_change", z = "num_domestic", template = "plotly_dark")
+pio.write_html(fig, "../../diagrams/bitcoin_close_price_vs_gold_change_vs_domestic_crimes_dark.html")
+
+sns.jointplot(x = "bitcoin_close_change", y = "num_domestic", data = df, kind = "hex", space = 0)
 
 #fig = px.scatter(df, x = "date", y = "financial_crimes", size = "count_negatives", color = "gold_value")
 #fig.show()
