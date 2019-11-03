5eee5a1ffddffacb429f6c944872744e5670c485
savage64 <56238155+savage64@users.noreply.github.com>
2019-11-03 22:09:30 +1100

    Update linear_regression.py

--- code/predicting/linear_regression.py
+++ code/predicting/linear_regression.py
@@ -11,7 +11,7 @@ import numpy as np
 raw_data = pd.read_csv("../../datasets/final/df_combined.csv",sep=",")
 data = raw_data.drop(raw_data.index[0])
 
-#converting
+#converting to floating point integers
 data["total_crimes"] = data["total_crimes"].astype(float)
 data["total_volume_of_tweets"] = data["total_volume_of_tweets"].astype(float)
 data["sent_negatives"] = data["sent_negatives"].astype(float)
@@ -20,30 +20,17 @@ data["financial_crimes"] = data["financial_crimes"].astype(float)
 data["gold_change"] = data["gold_change"].astype(float)
 data["bitcoin_close_change"] = data[["bitcoin_close_change"]].astype(float)
 
-X = data[["total_crimes", "total_volume_of_tweets", "sent_negatives", "sent_positives", "financial_crimes","gold_change"]
+X = data[["total_crimes", "total_volume_of_tweets", "sent_negatives", "sent_positives", \
+"financial_crimes","gold_change"]]
 y = data[["bitcoin_close_change"]]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-
 regr = LinearRegression().fit(X_train, y_train)
 
-# The coefficients
 print('Coefficients:')
 print(regr.coef_)
 # Use the model to predict y from X_test
 y_pred = regr.predict(X_test)
-# Root mean squared error
-mse = metrics.mean_squared_error(y_test, y_pred)
-print('Root mean squared error (RMSE):', sqrt(mse))
-# R-squared score: 1 is perfect prediction
-print('R-squared score:', metrics.r2_score(y_test, y_pred))
-
-#print()
-#note i (shilan) made cchanges most significantly adding in gold change
-#i think everything belowis irrelevant hayden has yet to confirm
-neigh = neighbors.KNeighborsRegressor(n_neighbors=4).fit(X_train, y_train)
-# Use the model to predict X_test
-y_pred = neigh.predict(X_test)
 # Root mean squared error
 mse = metrics.mean_squared_error(y_test, y_pred)
 print('Root mean squared error (RMSE):', sqrt(mse))
